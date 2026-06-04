8bfc3bdb218e0395813eb912e0e8bb98d1143754
nayanikavyas274-cmyk <nayanikavyas274@gmail.com>
2026-06-04 15:53:30 +0530

    Remove primary key from users table creation
    Removed primary key definition for the users table.

--- migrations/001_users.sql
+++ migrations/001_users.sql
@@ -4,8 +4,6 @@
 -- =====================================================
 
 CREATE TABLE IF NOT EXISTS users (
-  -- Primary Key
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
   
   -- Basic Information
   name TEXT,
