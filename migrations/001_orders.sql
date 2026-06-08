fe0ecc05e108e55b224e579dd5a06d1a8bcad418
Krishna Srivastava <154420450+Krishna41357@users.noreply.github.com>
2026-06-08 13:26:31 +0530

    Update 001_orders.sql

--- migrations/001_orders.sql
+++ migrations/001_orders.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS order_status_history;
 DROP TABLE IF EXISTS order_items;
 DROP TABLE IF EXISTS payment_transactions;
 DROP TABLE IF EXISTS orders;
-
+ 
 -- Create orders table
 CREATE TABLE IF NOT EXISTS orders (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
